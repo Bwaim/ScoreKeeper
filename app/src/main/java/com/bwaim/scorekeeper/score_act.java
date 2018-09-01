682a9f5658ba3c9d61deb29d7330f7e24b9f76bd
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-09-01 15:32:39 +0200

    feat: add the main Activity

---
+++ app/src/main/java/com/bwaim/scorekeeper/score_act.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2018 Fabien Boismoreau
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bwaim.scorekeeper;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bwaim.scorekeeper.configuration.ConfigurationViewModel;
+
+public class score_act extends AppCompatActivity {
+
+    private ConfigurationViewModel mConfigurationViewModel;
+
+    public static ConfigurationViewModel obtainViewModel(FragmentActivity activity) {
+        return ViewModelProviders.of(activity).get(ConfigurationViewModel.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score_act);
+
+        mConfigurationViewModel = obtainViewModel(this);
+    }
+}
