d84634fc9af79825d158aaead6fc29572e7da377
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-09-05 14:40:35 +0200

    feature: add main activity test

---
+++ app/src/androidTest/java/com/bwaim/scorekeeper/configuration/ScoreScreenTest.java
@@ -0,0 +1,69 @@
+/*
+ *    Copyright 2018 Fabien Boismoreau
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bwaim.scorekeeper.configuration;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.bwaim.scorekeeper.R;
+import com.bwaim.scorekeeper.ViewModelFactory;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Fabien Boismoreau on 04/09/2018.
+ * Test for the Score screen, the main screen
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ScoreScreenTest {
+
+    /**
+     * Check default value in {@link com.bwaim.scorekeeper.data.Configuration}
+     */
+    private final static String NAME_A = "Toto";
+    private final static String NAME_B = "Coder B";
+    private final static String INITIAL_SCORE = "25";
+    private final static String INITIAL_TIME = "01:30";
+
+    @Rule
+    public ActivityTestRule<ScoreActivity> mScoreActivityTestRule =
+            new ActivityTestRule<>(ScoreActivity.class);
+
+    @Before
+    public void resetState() {
+        ViewModelFactory.destroyInstance();
+    }
+
+    @Test
+    public void initialState() {
+        onView(withId(R.id.teamA)).check(matches(withText(NAME_A)));
+        onView(withId(R.id.teamB)).check(matches(withText(NAME_B)));
+        onView(withId(R.id.scoreTeamA)).check(matches(withText(INITIAL_SCORE)));
+        onView(withId(R.id.scoreTeamB)).check(matches(withText(INITIAL_SCORE)));
+        onView(withId(R.id.timer)).check(matches(withText(INITIAL_TIME)));
+    }
+}
